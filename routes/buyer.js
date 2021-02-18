7c11e602d0f79617dfb790631eef3524f845e458
Aviral Singh Chauhan <59026666+sAVItar02@users.noreply.github.com>
2021-02-18 20:04:39 +0530

    user authentication

--- routes/buyer.js
+++ routes/buyer.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const User = require("./../models/userModel");
 const router = new express.Router();
+const auth = require("./../middleware/userAuth");
 
 router.post("/buyer/signup", async (req, res) => {
   const newUser = new User(req.body);
   try {
     await newUser.save();
+    const token = await newUser.generateAuthToken();
     res.status(201).json({
       status: "success",
       data: {
@@ -23,9 +25,18 @@ router.post("/buyer/login", async (req, res) => {
       req.body.email,
       req.body.password
     );
+    const token = await user.generateAuthToken();
     res.status(200).json(user);
   } catch (e) {
     res.status(400).send(e);
+  }
+});
+
+router.get("/users/me", auth, async (req, res) => {
+  if (!req.user) {
+    res.status(401).send("Login");
+  } else {
+    res.status(201).send(req.user);
   }
 });
 
