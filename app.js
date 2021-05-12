9f5e3f299f62114f7036f4d9f57f1c6bc2d6a0a4
Shresth <shresth.tiwary2019@vitstudent.ac.in>
2021-05-12 16:39:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,13 +4,17 @@ const registerStoreRouter = require('./routes/store');
 const bodyParser = require('body-parser');
 
 const app = express();
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", '*');
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', '*');
   res.header("Access-Control-Allow-Credentials", true);
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', '*');
+    return res.status(200).json({});
+  }
   next();
 });
+
 app.use(require('cors')());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
