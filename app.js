9b1c63537962f6b112cd418ee0a6afb934564e20
Shresth <shresth.tiwary2019@vitstudent.ac.in>
2021-05-12 15:28:20 +0530

    add cors

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
-const express = require("express");
-const userRouter = require("./routes/buyer");
-const registerStoreRouter = require("./routes/store");
-const bodyParser = require("body-parser");
+const express = require('express');
+const userRouter = require('./routes/buyer');
+const registerStoreRouter = require('./routes/store');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', '*');
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', '*');
+    return res.status(200).json({});
+  }
+  next();
+});
 
-app.use(require("cors")());
+app.use(require('cors')());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
