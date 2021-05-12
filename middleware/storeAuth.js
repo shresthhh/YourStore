2342a9777dce8d0797a13767f5e5df4255711c49
Shresth <shresth.tiwary2019@vitstudent.ac.in>
2021-05-12 16:02:53 +0530

    Update with env variable

--- middleware/storeAuth.js
+++ middleware/storeAuth.js
@@ -1,13 +1,16 @@
-const jwt = require("jsonwebtoken");
-const Shop = require("./../models/storeModel");
+const jwt = require('jsonwebtoken');
+const Shop = require('./../models/storeModel');
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './config.env' });
 
 const auth = async (req, res, next) => {
   try {
-    const token = req.header("Authorization").replace("Bearer ", "");
-    const decoded = jwt.verify(token, "NarutoIsAmazing");
+    const token = req.header('Authorization').replace('Bearer ', '');
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
     const store = await Shop.findOne({
       _id: decoded._id,
-      "tokens.token": token,
+      'tokens.token': token,
     });
 
     if (!store) {
@@ -18,7 +21,7 @@ const auth = async (req, res, next) => {
     req.store = store;
     next();
   } catch (e) {
-    res.status(401).send("Please Authenticate");
+    res.status(401).send('Please Authenticate');
   }
 };
 
--- middleware/userAuth.js
+++ middleware/userAuth.js
@@ -1,10 +1,13 @@
 const jwt = require("jsonwebtoken");
 const User = require("./../models/userModel");
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './config.env' });
 
 const auth = async (req, res, next) => {
   try {
     const token = req.header("Authorization").replace("Bearer ", "");
-    const decoded = jwt.verify(token, "NarutoIsAmazing");
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
     const user = await User.findOne({
       _id: decoded._id,
       "tokens.token": token,
