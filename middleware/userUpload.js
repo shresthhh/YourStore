1f567c215c8c856c1d42cf481a91b482075d52ec
Shresth <shresth.tiwary2019@vitstudent.ac.in>
2021-05-18 14:30:10 +0530

    Add user uploads

---
+++ middleware/userUpload.js
@@ -0,0 +1,26 @@
+const multer = require('multer');
+
+const multerStorage = multer.diskStorage({
+  destination: (req, file, callback) => {
+    callback(null, 'public/img/users');
+  },
+  filename: (req, file, callback) => {
+    const ext = file.mimetype.split('/')[1];
+    callback(null, `user-${req.user.id}-${Date.now()}.${ext}`);
+  },
+});
+
+const multerFilter = (req, file, callback) => {
+  if (file.mimetype.startsWith('image')) {
+    callback(null, true);
+  } else {
+    return callback(new Error('Only images are allowed!'));
+  }
+};
+
+const upload = multer({
+  storage: multerStorage,
+  fileFilter: multerFilter,
+});
+
+exports.UploadUserPhoto = upload.single('photo');
